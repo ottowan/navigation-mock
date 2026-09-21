f117fd13e347f4e5612668c066034ebd77156709
ottowan <ottowan@gmail.com>
2026-09-21 14:27:38 +0700

    Switch portal typography to Noto Sans Thai Looped

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
-import '@fontsource/sarabun/400.css'
-import '@fontsource/sarabun/500.css'
-import '@fontsource/sarabun/600.css'
-import '@fontsource/sarabun/700.css'
+import '@fontsource-variable/noto-sans-thai-looped'
 import './styles.css'
+import './typography.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>)
